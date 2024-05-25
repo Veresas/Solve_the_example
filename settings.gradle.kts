8e8de35753d9f4f6b8ac7f0e2d00a4386678d0c6
Alexander Ozhered <ozhereda@mail.ru>
2024-05-25 19:21:03 +0300

    Init

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Solve_the_example"
+include(":app")
+ 
